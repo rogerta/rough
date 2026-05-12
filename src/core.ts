30a31ddf0b9d40997f534d0fb265c011cf365dc4
Roger Tawa <roger@tawacentral.net>
2026-05-11 23:15:17 -0400

    Group the fields of Options into related categories, easier to understand

--- src/core.ts
+++ src/core.ts
@@ -25,50 +25,28 @@ export interface DrawingSurface {
  * Options for drawing shapes.
  */
 export interface Options {
+  /////////////////////////////////////////////////////////////////////////////
+  // Options that affect all shape generation.
+
   /**
    * Maximum offset from the actual point in a line.
    */
   maxRandomnessOffset?: number;
   /**
    * Numerical value indicating how rough the drawing is. 0 is a perfect shape.
+   * Numbers above 10 are pretty useless.
    */
   roughness?: number;
   /**
    * Numerical value indicating how curvy the lines are when drawing a shape.
+   * Zero is a straight line, while higher values will make the shape more
+   * curvy.
    */
   bowing?: number;
-  /**
-   * Color of the stroke.
-   */
-  stroke?: string;
-  /**
-   * Width of the stroke.
-   */
-  strokeWidth?: number;
-  /**
-   * Curve fitting for curve-based shapes.
-   */
-  curveFitting?: number;
-  /**
-   * Tightness of the curve.
-   */
-  curveTightness?: number;
-  /**
-   * Number of steps for curve generation.
-   */
-  curveStepCount?: number;
-  /**
-   * Color used to fill the shape.
-   */
-  fill?: string;
   /**
    * Fill style.
    */
   fillStyle?: 'hachure' | 'solid' | 'zigzag' | 'cross-hatch' | 'dots' | 'dashed' | 'zigzag-line';
-  /**
-   * Weight of the fill lines.
-   */
-  fillWeight?: number;
   /**
    * Angle of hachure lines.
    */
@@ -78,18 +56,6 @@ export interface Options {
    */
   hachureGap?: number;
   /**
-   * Simplification factor for paths.
-   */
-  simplification?: number;
-  /**
-   * Dash offset for dashed fills.
-   */
-  dashOffset?: number;
-  /**
-   * Dash gap for dashed fills.
-   */
-  dashGap?: number;
-  /**
    * Zigzag offset for zigzag fills.
    */
   zigzagOffset?: number;
@@ -97,22 +63,6 @@ export interface Options {
    * Random seed for reproducible drawings.
    */
   seed?: number;
-  /**
-   * Line dash for the stroke.
-   */
-  strokeLineDash?: number[];
-  /**
-   * Line dash offset for the stroke.
-   */
-  strokeLineDashOffset?: number;
-  /**
-   * Line dash for the fill.
-   */
-  fillLineDash?: number[];
-  /**
-   * Line dash offset for the fill.
-   */
-  fillLineDashOffset?: number;
   /**
    * If true, multi-stroke will be disabled for the stroke.
    */
@@ -129,6 +79,76 @@ export interface Options {
    * Number of decimal places to fix coordinates to.
    */
   fixedDecimalPlaceDigits?: number;
+
+  /////////////////////////////////////////////////////////////////////////////
+  // Presenation properties.  These should eventually be removed and replaced
+  // with CSS properties.  This gets a little tricker for canvas.
+
+  /**
+   * Color of the stroke.
+   */
+  stroke?: string;
+  /**
+   * Width of the stroke.
+   */
+  strokeWidth?: number;
+  /**
+   * Color used to fill the shape.
+   */
+  fill?: string;
+  /**
+   * Weight of the fill lines.
+   */
+  fillWeight?: number;
+  /**
+   * Dash offset for dashed fills.
+   */
+  dashOffset?: number;
+  /**
+   * Dash gap for dashed fills.
+   */
+  dashGap?: number;
+  /**
+   * Line dash for the stroke.
+   */
+  strokeLineDash?: number[];
+  /**
+   * Line dash offset for the stroke.
+   */
+  strokeLineDashOffset?: number;
+  /**
+   * Line dash for the fill.
+   */
+  fillLineDash?: number[];
+  /**
+   * Line dash offset for the fill.
+   */
+  fillLineDashOffset?: number;
+
+  /////////////////////////////////////////////////////////////////////////////
+  // Options that affect shape generation that are curve-based, like ellipses,
+  // curves, arcs, and curves.
+
+  /**
+   * Curve fitting for curve-based shapes.
+   */
+  curveFitting?: number;
+  /**
+   * Tightness of the curve.
+   */
+  curveTightness?: number;
+  /**
+   * Number of steps for curve generation.
+   */
+  curveStepCount?: number;
+
+  /////////////////////////////////////////////////////////////////////////////
+  // Options that affect shape generation from SVG paths.
+
+  /**
+   * Simplification factor for paths.
+   */
+  simplification?: number;
   /**
    * Extra roughness gain for fill shapes.
    */
